102f325b1a116d9d8e26f32a9b5d2731244a6f30
lewisevans2007 <60709927+lewisevans2007@users.noreply.github.com>
2024-03-31 19:00:13 +0100

    Add python package support
    Signed-off-by: lewisevans2007 <60709927+lewisevans2007@users.noreply.github.com>

---
+++ setup.py
@@ -0,0 +1,16 @@
+# PMAP
+# A simple image format that is human readable and editable
+# GitHub: https://www.github.com/lewisevans2007/PMAP
+# Licence: GNU General Public License v3.0
+# By: Lewis Evans
+
+from setuptools import setup
+
+setup(
+    name="PMAP",
+    version="0.1",
+    packages=["PMAP"],
+    install_requires=[
+        "numpy",
+    ],
+)
